57abeb71c0c830535008f99038e9c94818425592
ilyaKiselev <kisel.ds@gmail.com>
2024-11-01 03:08:26 +0300

    add item_create

--- backend/sql_app/crud.py
+++ backend/sql_app/crud.py
@@ -20,12 +20,18 @@ def create_user(db: Session, user: schemas.UserCreate):
     db.refresh(db_user)
     return db_user
 
-# def delete_user(db: Session, user_id: int):
-#     db_user = db.query(User).filter(User.id == user_id).first()
-#     if db_user:
-#         db.delete(db_user)
-#         db.commit()
-#     return db_user
+def delete_user(db: Session, user_id: int):
+    db_user = db.query(models.User).filter(models.User.id == user_id).first()
+    if db_user:
+        db.delete(db_user)
+        db.commit()
+    return db_user
 
-def create_item(db:Session, item:schemas.ItemCreate):
-    db_item=models.Item()
+
+
+def create_item(db: Session, item: schemas.ItemCreate, user_id: int):
+    db_item = models.Item(title=item.title, description=item.description, owner_id=user_id)
+    db.add(db_item)
+    db.commit()
+    db.refresh(db_item)
+    return db_item
